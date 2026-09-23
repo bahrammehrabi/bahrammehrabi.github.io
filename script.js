13098d1199e31df062aa50c322bc0b2107ba9e57
bahrammehrabi <sb_mehrabi@yahoo.com>
2026-09-23 14:46:17 +0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-const btn=document.getElementById('lang');let de=false;btn.onclick=()=>{de=!de;btn.textContent=de?'EN':'DE';document.querySelectorAll('[data-en][data-de]').forEach(x=>x.textContent=de?x.dataset.de:x.dataset.en)};
-let ticking=false;
-ticking=true;}
-},{passive:true});
